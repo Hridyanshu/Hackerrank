f00a0b882dd2d31ca66c7cbb66f22fc790e87c2d
Hridyanshu <hridyanshusharma@gmail.com>
2020-05-12 20:28:38 +0530

    Java Loops II

--- JAVA/Loops2.java
+++ JAVA/Loops2.java
@@ -1,11 +1,7 @@
-/*We use the integers a, b,n and  to create the following series:
-(a+2(0).b),(a+2(0).b+2(1).b),…….,(a+2(0).b+2(1).b+….+2(n-1).b), where 2(x) represents 2 raised to the power x.
-You are given q queries in the form of a, b, and n. For each query, print the series corresponding to the given a, b, and n values as a single line of n space-separated integers.*/
-
 import java.util.*;
 import java.io.*;
 
-class Solution
+class Loops2
 {
     //The below method calculates the powers of any integer.
     int Power(int num, int pow)
@@ -45,20 +41,20 @@ class Solution
         //Creating object of Solution class.
         Solution object = new Solution();
 
-		//This loop determines that q series are printed.
+        //This loop determines that q series are printed.
         for(int i=0;i<q;i++)
         {
-			//Variable to get the first term of series.
-			int result1 = a[i] + object.Power(2,0)*b[i];
-			//This loop determines that n terms are printed.
+            //Variable to get the first term of series.
+            int result1 = a[i] + object.Power(2,0)*b[i];
+            //This loop determines that n terms are printed.
             for(int j=1;j<=n[i];j++)
             {
-				//Printing the terms of the series.
+                //Printing the terms of the series.
                 System.out.print((result1) + " ");
-				//Subsequent terms of the series are calculated as below.
-				result1 = result1 + object.Power(2,j)*b[i];
+                //Subsequent terms of the series are calculated as below.
+                result1 = result1 + object.Power(2,j)*b[i];
             }
-			System.out.println("");
+            System.out.println("");
         }
     }
 }
